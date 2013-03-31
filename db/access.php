41e455f201906e817753645bfd4c2b972d318a77
Bruno Sampaio <bens.sampaio@gmail.com>
2013-03-31 20:03:32 +0100

    v1.0.3

---
+++ db/access.php
@@ -0,0 +1,23 @@
+<?php
+$capabilities = array(
+ 
+    'block/applications_list:myaddinstance' => array(
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => array(
+            'user' => CAP_ALLOW
+        ),
+        'clonepermissionsfrom' => 'moodle/my:manageblocks'
+    ),
+ 
+    'block/applications_list:addinstance' => array(
+        'riskbitmask' => RISK_SPAM | RISK_XSS,
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_BLOCK,
+        'archetypes' => array(
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ),
+        'clonepermissionsfrom' => 'moodle/site:manageblocks'
+    )
+);
